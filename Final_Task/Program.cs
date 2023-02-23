169abcf831ada92f105bdd0d128d2db12bd06cc6
Sergei Matveev <msergne@gmail.com>
2023-02-23 13:01:37 +0300

    Completed code

--- Final_Task/Program.cs
+++ Final_Task/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача: Написать программу, которая из имеющегося массива строк формирует массив из строк, 
+// длина которых меньше либо равна 3 символа.
+// Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма.
+
+string[] array1 = {"Hello", "2", "world", ":-)", "123"};
+int wordLength = 3;
+
+string[] ShortArray(string[] array)
+{
+    string[] result = new string[array.Length];
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= wordLength)
+        {
+            result[count] = array[i];
+            count++;
+        }
+    }
+    Array.Resize(ref result, count);
+    return result;
+}
+
+void PrintArray(string[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}");
+    }
+    Console.Write("]");
+}
+
+PrintArray(array1);
+Console.Write(" -> ");
+string[] resultArray = ShortArray(array1);
+PrintArray(resultArray);
